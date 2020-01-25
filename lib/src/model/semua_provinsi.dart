4400e40a9e465e2f5947605ade80a84cdffb0251
Jhoan <jhoan.river@inmotion.co.id>
2020-01-25 18:44:49 +0700

    jhoan  / add class ApiResultModel

--- lib/src/model/semua_provinsi.dart
+++ lib/src/model/semua_provinsi.dart
@@ -1,15 +1,55 @@
 
 
 
-class SemuaProvinsi{
-  String id;
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+class ApiResultModel{
+
+  bool error;
+  String message;
+  List<SemuaProvinsi> semuaprovinsi;
+
+  ApiResultModel({this.error,this.message,this.semuaprovinsi});
+
+
+  ApiResultModel.fromJson(Map<String, dynamic> json){
+    error = json['error'];
+    message = json['message'];
+
+    if(json['semuaprovinsi'] != null){
+      semuaprovinsi = new List<SemuaProvinsi>();
+
+      json['semuaprovinsi'].forEach((v){
+        semuaprovinsi.add(new SemuaProvinsi.fromJson(v));
+      });
+
+    }
+  }
+
+}
+
+
+
+class SemuaProvinsi extends Equatable{
   String nama;
 
+  SemuaProvinsi({@required this.nama});
 
-  SemuaProvinsi(parseJson){
-    id = parseJson['id'];
-    nama = parseJson['nama'];
+  @override
+  List<Object> get props => [nama];
+
+  @override
+  String toString() {
+    return 'SemuaProvinsi{ nama: $nama}';
   }
+
+
+  SemuaProvinsi.fromJson(Map<String, dynamic> json){
+    nama = json['nama'];
+
+  }
+
 
 
 
